666dc1ed77088c1d5528a4f3ddb3c660163448f8
kormannj <33202837+kormannj@users.noreply.github.com>
2017-12-19 14:19:59 -0200

    Update ex069.py

--- ex069.py
+++ ex069.py
@@ -1,8 +1,27 @@
 #Leitura de cadastro de pessoas
+print('-' * 30)
+
 continua = 'S'
-mais18 = qt_homens = mulheres_menos20 = 0
+pessoas_mais18 = qt_homens = mulheres_menos20 = 0
 while continua == 'S':
     idade = int(input('Digite a idade: '))
-    sexo = input('Digite o sexo (M/F): ').strip().upper()[0]
-    continua = input('Deseja continuar (S/N)? ').strip().upper()[0]
-    mais18 = mais18 + 1 if idade > 18
+    sexo = ' '
+    while sexo not in ('M', 'F'):
+        sexo = input('Digite o sexo (M/F): ').strip().upper()[0]
+
+    if idade > 18:
+        pessoas_mais18 += 1
+    if sexo == 'M':
+        qt_homens += 1
+    if sexo == 'F' and idade < 20:
+        mulheres_menos20 += 1
+
+    print('-' * 30)
+    continua = ' '
+    while continua not in ('S', 'N'):
+        continua = input('Deseja continuar (S/N)? ').strip().upper()[0]
+
+print('====== FIM DO PROGRAMA ======')
+print(f'Total de pessoas com mais de 18 anos: {pessoas_mais18}.')
+print(f'Ao todo temos {qt_homens} homens cadastrado(s).')
+print(f'Temos {mulheres_menos20} mulheres com menos de 20 anos.')

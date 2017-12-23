c34a882827ba5541118a1a28bd88b0426976eea5
kormannj <33202837+kormannj@users.noreply.github.com>
2017-12-23 15:43:21 -0200

    Update ex060.py

--- ex060.py
+++ ex060.py
@@ -1,18 +1,13 @@
-#Cálculo do fatorial de um número
+#Cálculo do fatorial de um número com WHILE
 num = int(input('Digite um número: '))
 num_orig = num
 
 if num <= 0:
     print('O número deve ser maior que zero.')
 else:
-    cont = 0
+    fatorial = 1
     while num >= 1:
-        if cont == 0:
-            fatorial = num
-        else:
-            fatorial = fatorial * num
-
-        cont += 1
+        fatorial *= num
         num -= 1
 
-print('O fatorial do número {} é {}.'.format(num_orig, fatorial))
+print('O fatorial do número {} é {}.\n'.format(num_orig, fatorial))
